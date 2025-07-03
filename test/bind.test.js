bb2b5e57513b9658f739e9e116bcfd266602f5f6
Auke van Slooten <auke@muze.nl>
2025-07-03 14:33:24 +0200

    fixed tests, switched to data-bind-field/list/map and :root instead of #root

--- test/bind.test.js
+++ test/bind.test.js
@@ -4,9 +4,9 @@ import {bind} from '../src/bind.mjs'
 describe('bind can', () => {
   it('render simple list', (done) => {
     const source = `
-  <ul data-bind="menu">
+  <ul data-bind-list="menu">
     <template>
-<li><a data-bind="item"></a></li></template></ul>`
+<li><a data-bind-field="item"></a></li></template></ul>`
     const data = signal({
       menu: [
         {
@@ -24,35 +24,35 @@ describe('bind can', () => {
       ]
     })
     document.body.innerHTML = source
-    const databind = bind({
-      container: document.body,
-      root: data
-    })
-    const rendered = `
-  <ul data-bind="menu">
+      const databind = bind({
+        container: document.body,
+        root: data
+      })
+      const rendered = `
+  <ul data-bind-list="menu">
     <template>
-<li><a data-bind="item"></a></li></template>
-<li data-bind-key="0"><a data-bind="menu.0.item" href="#item1">item 1</a></li>
-<li data-bind-key="1"><a data-bind="menu.1.item" href="#item2">item 2</a></li></ul>`
-    setTimeout(() => {
-      try {
-        expect(document.body.innerHTML).toBe(rendered)
-        done()
-      } catch(error) {
-        done(error)
-      } finally {
-        databind.destroy()
-      }
-    }, 10)
+<li><a data-bind-field="item"></a></li></template>
+<li data-bind-key="0"><a data-bind-field="menu.0.item" href="#item1">item 1</a></li>
+<li data-bind-key="1"><a data-bind-field="menu.1.item" href="#item2">item 2</a></li></ul>`
+      setTimeout(() => {
+        try {
+          expect(document.body.innerHTML).toBe(rendered)
+          done()
+        } catch(error) {
+          done(error)
+        } finally {
+          databind.destroy()
+        }
+      }, 10)
   })
   it('render matching templates', (done) => {
     const data = signal({
       foo: 1,
       bar: 'bar'
     })
-    const source = `<div data-bind="foo">
+    const source = `<div data-bind-field="foo">
         <template data-bind-match="1">
-          <div data-bind="bar"></div>
+          <div data-bind-field="bar"></div>
         </template>
       </div>`
     document.body.innerHTML = source
@@ -60,12 +60,12 @@ describe('bind can', () => {
       container: document.body,
       root: data
     })
-    const rendered = `<div data-bind=\"foo\">
+    const rendered = `<div data-bind-field=\"foo\">
         <template data-bind-match=\"1\">
-          <div data-bind=\"bar\"></div>
+          <div data-bind-field=\"bar\"></div>
         </template>
       
-          <div data-bind=\"#root.bar\">bar</div>
+          <div data-bind-field=\":root.bar\">bar</div>
         </div>`
     setTimeout(() => {
       try {
