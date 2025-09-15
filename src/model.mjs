1fb26f0ac92ca15d8b7d85de805eecd0990c054b
Auke van Slooten <auke@muze.nl>
2025-09-15 11:52:37 +0200

    added docblocks made sort() more generic

--- src/model.mjs
+++ src/model.mjs
@@ -42,6 +42,14 @@ export function model(options) {
 	return new SimplyFlowModel(options)
 }
 
+/**
+ * Returns a function for model.addEffect that sorts the input data
+ * 
+ * Options:
+ * - direction (string) default 'asc' - change to 'desc' to sort in descending order
+ * - sortBy (string) (optional) - used by the default sorting function to select the property to sort on
+ * - sortFn (function) (required - set by default) - the sort function to use
+ */
 export function sort(options={}) {
 	return function(data) {
 		// initialize the sort options, only gets called once
@@ -86,6 +94,16 @@ export function sort(options={}) {
 	}
 }
 
+/**
+ * Returns a function for model.addEffect that implements paging
+ * for the input data. It will return a slice of the data matching
+ * the page and pageSize options.
+ * 
+ * Options:
+ * - page (int) default 1 - which page to show, starts at 1
+ * - pageSize (int) default 20 - how many items in a single page
+ * - max (int) (calculated) - how many pages in total
+ */
 export function paging(options={}) {
 	return function(data) {
 		// initialize the paging options
@@ -111,9 +129,20 @@ export function paging(options={}) {
 	}
 }
 
+/**
+ * Returns a function for model.addEffect that filters rows from the data,
+ * using a custom filter function `options.matches`
+ * 
+ * Options:
+ * - name (string) (required) - the name of this filter, must be unique
+ * - matches (function) (required) - the filter function to apply to the data
+ */
 export function filter(options) {
 	if (!options?.name || typeof options.name!=='string') {
 		throw new Error('filter requires options.name to be a string')
+	}
+	if (this.state.options[options.name]) {
+		throw new Error('a filter with this name already exists on this model')
 	}
 	if (!options.matches || typeof options.matches!=='function') {
 		throw new Error('filter requires options.matches to be a function')
@@ -128,6 +157,16 @@ export function filter(options) {
 	}
 }
 
+/**
+ * Returns a function for model.addEffect that filters the data to only contain
+ * columns (properties) that aren't hidden. Automatically runs again if any columns
+ * hidden property changes.
+ * 
+ * Options:
+ * - columns (object) (required) - an object with properties describing each column. Each 
+ * property must be an object with an optional `hidden` property. If set to a truthy value,
+ * and property in the dataset with the same name, will be filtered out.
+ */
 export function columns(options={}) {
 	if (!options
 		|| typeof options!=='object'
@@ -150,42 +189,67 @@ export function columns(options={}) {
 	}
 }
 
+/**
+ * Returns a function for use with model.addEffect, with the given options set
+ * as model.options.scroll. The effect will return a slice of the input data, which
+ * makes it easy to render just a part (slice) of the whole data.
+ * 
+ * Options are:
+ * - offset (int) default 0 (optional) - the offset in the data to start the slice
+ * - rowCount (int) default 20 (optional / calculated) - the number of rows in the slice
+ * - rowHeight (int) default 26 (optional) - the height of a single row in pixels
+ * - itemsPerRow (int) default 1 (optional) - the number of items on a single row
+ * - size (int) default data.current.length (calculated) - how many rows inside data.current before slicing
+ * - scrollbar (HTMLElement) defualt null (optional) - if set, an effect is added to update this elements 
+ * 	 height if data.current.length changes
+ * - container (HTMLElement) default null (optional) - if set, a scroll listener is added to this element, 
+ *   which will update the options.offset signal and trigger the slice effect. It will also set the rowCount.
+ */
 export function scroll(options) {
-	if (!options.container) {
-		throw new Error('scroll needs a container option to calculate the number of visible rows')
-	}
 
 	return function(data) {
 		this.state.options.scroll = Object.assign({
 			offset: 0,
-			rowHeight: 26
+			rowHeight: 26,
+			rowCount: 20,
+			itemsPerRow: 1,
+			size: data.current.length
 		}, options)
 		const scrollOptions = this.state.options.scroll
 
-		const scrollbar = scrollOptions.scrollbar || scrollOptions.container.querySelector('[data-flow-scrollbar]')
-		if (!scrollbar) {
-			throw new Error('scroll needs a scrollbar, or an element with the attribute "data-flow-scrollbar"')
-		}
-		scrollOptions.container.addEventListener('scroll', (evt) => {
-			scrollOptions.offset = Math.floor(scrollOptions.container.scrollTop/scrollOptions.rowHeight)
-			console.log('scrolling',scrollOptions.offset)
-		})
+		const scrollbar = scrollOptions.scrollbar 
+			|| scrollOptions.container?.querySelector('[data-flow-scrollbar]')
+		if (scrollbar) {
+			if (scrollOptions.container) {
+				scrollOptions.container.addEventListener('scroll', (evt) => {
+					scrollOptions.offset = Math.floor(scrollOptions.container.scrollTop
+						/ (scrollOptions.rowHeight*scrollOptions.itemsPerRow)
+					)
+				})
+			}
 
-		effect(() => {
-			let size = data.current.length * scrollOptions.rowHeight
-			scrollbar.style.height = size + 'px'
-		})
+			effect(() => {
+				scrollOptions.size = data.current.length * scrollOptions.rowHeight
+				scrollbar.style.height = scrollOptions.size + 'px'
+			})
+		}
 
 		return effect(() => {
-			scrollOptions.rows = Math.ceil(scrollOptions.container.getBoundingClientRect().height / scrollOptions.rowHeight)
+			if (scrollOptions.container) {
+				//TODO: add a resize listener so that if the size of the container
+				// changes, the rowCount is calculated again
+				scrollOptions.rowCount = Math.ceil(
+					scrollOptions.container.getBoundingClientRect().height 
+					/ scrollOptions.rowHeight
+				)
+			}
 			scrollOptions.data = data.current
 			let start = Math.min(scrollOptions.offset, data.current.length-1)
-			let end   = start + scrollOptions.rows
+			let end   = start + scrollOptions.rowCount
 			if (end > data.current.length) {
 				end   = data.current.length
-				start = end - scrollOptions.rows
+				start = end - scrollOptions.rowCount
 			}
-			console.log('virtual scroll',start,end)
 			return data.current.slice(start, end)
 		})
 	}
