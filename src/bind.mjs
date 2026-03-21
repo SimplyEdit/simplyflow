cc745beb2a046b1dd46646da03914c1087441297
Auke van Slooten <auke@muze.nl>
2026-03-21 13:38:59 +0100

    template match is now exact string match list now accepts a single value (non-array) as well

--- src/bind.mjs
+++ src/bind.mjs
@@ -331,7 +331,7 @@ class SimplyBind
                 } else if (matches===':notempty' && currentItem) {
                     return t
                 }
-                if (strItem.match(matches)) {
+                if (strItem == matches) {
                     return t
                 }
             }
@@ -422,7 +422,11 @@ export function getValueByPath(root, path)
     part = parts.shift()
     while (part && curr) {
         part = decodeURIComponent(part)
-        curr = curr[part]
+        if (part=='0' && !Array.isArray(curr)) {
+            // ignore so that data-flow-list="nonarray" will work
+        } else {
+            curr = curr[part]
+        }
         part = parts.shift()
     }
     return curr
